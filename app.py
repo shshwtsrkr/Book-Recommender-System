c6aa141eac5c3107bf39dbd118283f8ee4495b3d
Shashwat Sarkar <shashwatsarkar22@gmail.com>
2022-10-01 15:37:18 +0530

    Added a second webpage for enabling recommendation

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+scores = pickle.load(open('scores.pkl', 'rb'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +18,32 @@ def index():
                            votes     = (list(popular_df['num_ratings'].values)),
                            ratings   = (list(popular_df['avg_ratings'].values))
                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['post'])
+def recommend():
+    user_input = request.form.get('uip')
+
+    #fetch the exact index of the book
+    index_fetch = np.where(pt.index == user_input)[0][0]
+    sim_items = sorted(list(enumerate(scores[index_fetch])), key = lambda x:x[1], reverse = True)[1:6]
+    
+    data = []
+
+    for i in sim_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    return render_template('recommend.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
